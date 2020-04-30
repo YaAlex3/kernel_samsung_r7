089596525a049a94aad201ba133b2b3432e8fa63
Sami Tolvanen <samitolvanen@google.com>
2020-04-29 17:28:46 -0700

    ANDROID: kbuild: don't select LD_DEAD_CODE_DATA_ELIMINATION with LTO
    Instead of enabling -gc-sections, which can break the kernel by dropping
    sections that are actually needed, just merge the sections also when LTO
    is enabled.
    
    Bug: 145297900
    Change-Id: I86aecd543d303eea4d30e7ce49cf5fcb3ed24307
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    [lazerl0rd: Adjust to avoid breaking DCE.]
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -68,14 +68,11 @@
  * RODATA_MAIN is not used because existing code already defines .rodata.x
  * sections to be brought in with rodata.
  */
-#ifdef CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
+#if defined(CONFIG_LD_DEAD_CODE_DATA_ELIMINATION) || defined(CONFIG_LTO_CLANG)
 #define TEXT_MAIN .text .text.[0-9a-zA-Z_]*
 #define TEXT_CFI_MAIN .text.cfi .text.[0-9a-zA-Z_]*.cfi
-#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L_MergedGlobals*
-#define SDATA_MAIN .sdata .sdata.[0-9a-zA-Z_]*
-#define RODATA_MAIN .rodata .rodata.[0-9a-zA-Z_]*
-#define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .bss..compoundliteral* .bss..L_MergedGlobals*
-#define SBSS_MAIN .sbss .sbss.[0-9a-zA-Z_]*
+#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L*
+#define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .data..compoundliteral* .data..L*
 #else
 #define TEXT_MAIN .text
 #define TEXT_CFI_MAIN .text.cfi
@@ -83,6 +80,20 @@
 #define SDATA_MAIN .sdata
 #define RODATA_MAIN .rodata
 #define BSS_MAIN .bss
+#define SBSS_MAIN .sbss
+#endif
+
+/*
+ * RODATA_MAIN is not used because existing code already defines .rodata.x
+ * sections to be brought in with rodata.
+ */
+#ifdef CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
+#define SDATA_MAIN .sdata .sdata.[0-9a-zA-Z_]*
+#define RODATA_MAIN .rodata .rodata.[0-9a-zA-Z_]*
+#define SBSS_MAIN .sbss .sbss.[0-9a-zA-Z_]*
+#else
+#define SDATA_MAIN .sdata
+#define RODATA_MAIN .rodata
 #define SBSS_MAIN .sbss
 #endif
 
