e95ce6461543a79a0116d0b12ef4fe84a01fa6b9
Alexander <yaalex113@gmail.com>
2022-06-07 11:38:48 +0000

    Revert "rcu: Make rcu_idle_enter() rely on callers disabling irqs"
    This reverts commit 1fe7ecd0ec7d42ca16f9299b149fea0c7df279d4.

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -826,8 +826,11 @@ static void rcu_eqs_enter(bool user)
  */
 void rcu_idle_enter(void)
 {
-	RCU_LOCKDEP_WARN(!irqs_disabled(), "rcu_idle_enter() invoked with irqs enabled!!!");
+	unsigned long flags;
+
+	local_irq_save(flags);
 	rcu_eqs_enter(false);
+	local_irq_restore(flags);
 }
 EXPORT_SYMBOL_GPL(rcu_idle_enter);
 
